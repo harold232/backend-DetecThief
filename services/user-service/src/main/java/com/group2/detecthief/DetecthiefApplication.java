78f671eca4baf4c031da07e0902cacd254433b70
Harold Aquino <harold.aquino@unmsm.edu.pe>
2025-07-08 00:19:15 -0500

    Change: Fix laodenv

--- services/user-service/src/main/java/com/group2/detecthief/DetecthiefApplication.java
+++ services/user-service/src/main/java/com/group2/detecthief/DetecthiefApplication.java
@@ -10,6 +10,7 @@ public class DetecthiefApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
 				.load();
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
