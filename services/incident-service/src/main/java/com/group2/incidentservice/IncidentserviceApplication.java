f135ccf0ef83018fe6e8f3273bea372ce57ef0f9
Harold Aquino <harold.aquino@unmsm.edu.pe>
2025-07-08 00:33:09 -0500

    Change config deploy

--- services/incident-service/src/main/java/com/group2/incidentservice/IncidentserviceApplication.java
+++ services/incident-service/src/main/java/com/group2/incidentservice/IncidentserviceApplication.java
@@ -9,7 +9,7 @@ public class IncidentserviceApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure()
-				.directory("../")
+				.ignoreIfMissing()
 				.load();
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
