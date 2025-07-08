8a5ff699d9022305d85a98c0dcd00303c0d8be07
Harold Aquino <harold.aquino@leasein.pe>
2025-07-08 00:09:04 -0500

    Change env files

--- services/user-service/src/main/java/com/group2/detecthief/DetecthiefApplication.java
+++ services/user-service/src/main/java/com/group2/detecthief/DetecthiefApplication.java
@@ -10,7 +10,6 @@ public class DetecthiefApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure()
-				.directory("../")
 				.load();
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
